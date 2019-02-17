4dbbfc4e2c3bd09e44d2af3c6bb557cf5163a667
Michael Gatewood <michael@mngatewood.com>
2019-02-17 04:20:33 -0700

    Add conditional fetch * add fetch urls dependant on type * update id based on type

--- src/actions/foodActions.js
+++ src/actions/foodActions.js
@@ -11,7 +11,10 @@ export const fetchFoods = () => dispatch =>  {
 }
 
 export const createFood = (foodData) => dispatch => {
-  fetch('https://trigger-backend.herokuapp.com/api/v1/foods', {
+  let foodUrl = `https://trigger-backend.herokuapp.com/api/v1/${foodData.type}s`
+  let foodEntryUrl = `https://trigger-backend.herokuapp.com/api/v1/${foodData.type}_entries`
+
+  fetch(foodUrl, {
     method: 'POST',
     headers: {
       'content-type': 'application/json'
@@ -21,24 +24,36 @@ export const createFood = (foodData) => dispatch => {
     .then(response => response.json())
       .then(response => {
         if (foodData.time !== "") {
-          fetch('https://trigger-backend.herokuapp.com/api/v1/food_entries/', {
+          fetch(foodEntryUrl, {
             method: 'POST',
             headers: {
               'content-type': 'application/json'
             },
-            body: JSON.stringify({ food_id: response.id, time: foodData.time })
+            body: JSON.stringify({ [`${foodData.type}_id`]: response.id, time: foodData.time })
           })
           .then(response => response.json())
-          return response
+          return response  // this is only returning a status...
         }
         else {
           return response
         }
       })
-      .then(food => dispatch({
+      .then(food => dispatch({  // so food here is only a status...
         type: NEW_FOOD,
-        payload: food
+        payload: food  // which means our payload isn't a food, which breaks Foods.js componentWillReceiveProps
       })
   );
 
  }
+
+// We either need build a food object like this: 
+// payload: {
+//   id: response.id,
+//   type: foodData.type,
+//   name: foodData.name,
+//   time: foodData.time
+// }
+
+// or we need to ask BE to return the food_entry/reaction_entry in the response and send it as the payload
+
+// for the time being, I prefer the first option
