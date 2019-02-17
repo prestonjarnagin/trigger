30dd6b2027deece41836c855b203c2e8dba4f32b
Michael Gatewood <michael@mngatewood.com>
2019-02-17 04:24:12 -0700

    Add reaction to foodForm * clear foodForm fields on update * add function to show/hide form based on state * render status message from response.status * update tabs and placeholder based on state * udpate styling for all components

--- src/components/FoodForm.js
+++ src/components/FoodForm.js
@@ -10,10 +10,25 @@ class FoodForm extends Component {
       type: 'food',
       name: '',
       time: ''
-    };
+      };
     this.onChange = this.onChange.bind(this);
     this.onSubmit = this.onSubmit.bind(this);
+    this.addFood = this.addFood.bind(this);
+    this.addReaction = this.addReaction.bind(this);
+    this.renderResponse = this.renderResponse.bind(this);
     this.formContainer = React.createRef();
+    this.fieldsContainer = React.createRef();
+    this.responseContainer = React.createRef();
+  }
+
+  componentWillUpdate() {
+    if (this.props.displayAddForm === false) {
+      this.setState({
+        type: 'food',
+        name: '',
+        time: ''
+      })
+    }
   }
 
   componentDidUpdate() {
@@ -21,9 +36,21 @@ class FoodForm extends Component {
   }
 
   toggleDisplay() {
-    this.props.displayAddForm ?
-      this.formContainer.current.style.bottom = "60px" : 
-      this.formContainer.current.style.bottom = "-280px"
+    if(this.props.displayAddForm) {
+      this.formContainer.current.style.bottom = "60px";
+    } else {
+      this.formContainer.current.style.bottom = "-280px";
+      this.fieldsContainer.current.style.visibility = "visible";
+      this.responseContainer.current.style.visibility = "hidden";
+    }
+  }
+
+  addFood() {
+    this.setState({ type: "food"})
+  }
+
+  addReaction() {
+    this.setState({ type: "reaction" })
   }
 
   onChange(event) {
@@ -36,42 +63,65 @@ class FoodForm extends Component {
     const food = {
       type: this.state.type,
       name: this.state.name,
-      time: this.state.time
+      time: this.state.time  // need to add a date helper function to convert simple time to Unix
     }
     this.props.createFood(food);
+    this.renderResponse(this.props.foodResponse); // this doesn't work because createFood is async, need to implement async/await?  
+  }
+
+  renderResponse(response) {
+    this.fieldsContainer.current.style.visibility = "hidden";
+    this.responseContainer.current.style.visibility = "visible";
   }
 
   render() {
+
+    const selectedTab = (type) => {
+      return type === this.state.type ? "selected-tab" : ""
+    }
+
+    const namePlaceholder = () => {
+      let type = this.state.type
+      let capType = type.charAt(0).toUpperCase() + type.slice(1)
+      return `Enter Name of ${capType}`;
+    }
+
     return(
       <div id="food-form-container" ref={this.formContainer}>
         <div className="food-form-tabs-container">
-          <div className="food-form-tab add-food-tab">
+          <div className={"food-form-tab add-food-tab " + selectedTab("food")} 
+               onClick={this.addFood}>
             <h4>Add Food</h4>
           </div>
-          <div className="food-form-tab add-reaction-tab">
+          <div className={"food-form-tab add-reaction-tab " + selectedTab("reaction")} 
+               onClick={this.addReaction}>
             <h4>Add Reaction</h4>
           </div>
         </div>
-        <div id="food-form-fields-container">
+        <div id="food-form-fields-container" ref={this.fieldsContainer}>
           <form onSubmit={this.onSubmit}>
             <div>
               <input type="text"
                      name="name"
-                     value={this.state.title}
-                     placeholder="Enter Name of Food"
+                     value={this.state.name}
+                     placeholder={namePlaceholder()}
                      onChange={this.onChange}
               />
             </div>
             <div>
               <input type="text"
                      name="time"
-                     value={this.state.body}
-                     placeholder="Time"
+                     value={this.state.time}
+                     placeholder="Enter Time"
                      onChange={this.onChange}
               />
               <button type="submit"> Submit </button>
             </div>
           </form>
+        </div>
+        <div id="food-form-response-container" ref={this.responseContainer}>
+          <h3 id="food-form-response">{this.props.foodResponse}</h3>
+          <h3>Click "X" below to close.</h3>
         </div>
       </div>
     )
@@ -83,6 +133,7 @@ FoodForm.propTypes = {
 }
 const mapStateToProps = state => ({
   displayAddForm: state.foodForm.display,
+  foodResponse: state.foods.item.status
 });
 
 export default connect(mapStateToProps, {createFood} )(FoodForm);
