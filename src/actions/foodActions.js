5807f9efce91490257af8ca4ff8720ccc31a8500
Cesar Jolibois <diazcd85@gmail.com>
2019-02-17 18:15:22 -0700

    refactor to the foodactions and corrected small bugs

--- src/actions/foodActions.js
+++ src/actions/foodActions.js
@@ -13,7 +13,6 @@ export const fetchFoods = () => dispatch =>  {
 
 export const createFood = (foodData) => dispatch => {
   let foodUrl = `https://trigger-backend.herokuapp.com/api/v1/${foodData.type}s`
-  let foodEntryUrl = `https://trigger-backend.herokuapp.com/api/v1/${foodData.type}_entries`
 
   fetch(foodUrl, {
     method: 'POST',
@@ -25,29 +24,39 @@ export const createFood = (foodData) => dispatch => {
     .then(response => response.json())
       .then(response => {
         if (foodData.time !== "") {
-          fetch(foodEntryUrl, {
-            method: 'POST',
-            headers: {
-              'content-type': 'application/json'
-            },
-            body: JSON.stringify({
-              [`${foodData.type}_id`]: response.id,
-              time: foodData.time
-            })
-          })
-          .then(response => response.json())
-          foodData.id = response.id
-          foodData.status = response.status
+          foodEntryPost(response, foodData)
+          chanceFoodData(response, foodData)
+          return foodData
         }
         else {
-          foodData.id = response.id
-          foodData.status = response.status
+          chanceFoodData(response, foodData)
+          return foodData
         }
       })
-      .then(food => dispatch({  // so food here is only a status...
+      .then(food => dispatch({
         type: NEW_FOOD,
-        payload: food  // which means our payload isn't a food, which breaks Foods.js componentWillReceiveProps
+        payload: food
       })
   );
-
  }
+
+const foodEntryPost = (response, foodData) => {
+  let foodEntryUrl = `https://trigger-backend.herokuapp.com/api/v1/${foodData.type}_entries`
+
+  fetch(foodEntryUrl, {
+    method: 'POST',
+    headers: {
+      'content-type': 'application/json'
+    },
+    body: JSON.stringify({
+      [`${foodData.type}_id`]: response.id,
+      time: foodData.time
+    })
+  })
+  .then(response => response.json())
+}
+
+const chanceFoodData = (response, foodData) => { // this method is only updating the status of the foodData variable
+  foodData.id = response.id
+  foodData.status = response.status
+}
