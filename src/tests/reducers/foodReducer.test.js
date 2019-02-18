1f3a4bc5e2f298a006befab7eacddbe23a928883
Cesar Jolibois <diazcd85@gmail.com>
2019-02-18 15:19:31 -0700

    WIP testing food reducer

--- src/tests/reducers/foodReducer.test.js
+++ src/tests/reducers/foodReducer.test.js
@@ -1,3 +1,4 @@
+import { FETCH_FOODS, NEW_FOOD, DESTROY_FOOD } from '../../actions/types'
 import foodReducer from '../../reducers/foodReducer'
 
 describe('Food Reducer test', () => {
@@ -7,5 +8,14 @@ describe('Food Reducer test', () => {
         item: {},
         changedItem: {}
     })
+  })
+
+  it('should return and food/reaction item', () => {
+    let food = { id: "68",
+                type: "food",
+                name: "Peas"
+              }
+    expect(foodReducer(undefined, {type: NEW_FOOD}))
+
   })
 })
