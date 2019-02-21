d8f915a4a54adc5b9796d098175e27b01d96bba6
Michael Gatewood <michael@mngatewood.com>
2019-02-20 21:05:46 -0700

    Fix typos * fixes typos in help messages * adds minor changes to CSS

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -87,24 +87,36 @@ class Nav extends Component {
         <div id="home-help-container" 
              className="help-container"
              ref={this.homeHelp} >
-          <h2>Foods and Reactions</h2>
-          <p>Here is some information you might find useful.</p>
-          <h4>What is food?</h4>
-          <p>Food is...</p>
-          <h4>What is a reaction?</h4>
-          <p>A reaction is...</p>
-          <h4>What are all these buttons?</h4>
+          <h3>Foods and Reactions</h3>
+          <p>This page displays foods and reactions that were added for the selected date.
+          You can change the selected date by clicking the arrow buttons above or by clicking on the previous or next date, indicated by an underline.</p>
+          <h4>Updating and Deleting</h4>
+          <p>You can delete any food (indicated by gray background) or reaction (indicated by red background) by clicking the X button next to the name of the food or reaction.
+          The name and time for a food or reaction can be updated by clicking the pencil icon.</p>
+          <h4>Adding Foods and Reactions</h4>
+          <p>Click the green + button below to add a food or reaction.  Select Food or Reaction, enter the name and time, and click Submit.  
+          A message should appear indicating that your food or reaction was saved.  Click the red X button to cancel the form.</p>
+          <p>Click the graph button below to see detailed information about your added reactions.</p>
         </div>
         <div id="analytics-help-container" 
              className="help-container"
              ref={this.analyticsHelp} >
-          <h2>Reactions and Potential Triggers</h2>
-          <p>Here is some information you might find useful.</p>
-          <h4>What are occurrences?</h4>
-          <p>An occurrence is...</p>
-          <h4>What are potential triggers?</h4>
-          <p>A potential trigger is...</p>
-          <h4>What are all these buttons?</h4>
+          <h3>Reactions and Potential Triggers</h3>
+          <p>This screen displays details about reactions that have been added.
+          If you have added more than one reaction, you can view additional reactions by clicking the arrows on each side of the name of the condition above.
+          The following provides a brief description of the fields on this page.</p>
+          <h4>Last Week</h4>
+          <p>The number of occurrences in the past 7 days.</p>
+          <h4>Last Two Weeks</h4>
+          <p>The number of occurrences in the past 14 days.</p>
+          <h4>This Month</h4>
+          <p>The number of occurrences in the past 30 days.</p>
+          <h4>Last Month</h4>
+          <p>The number of occurrences in the past 31-60 days.</p>
+          <h4>Potential Triggers</h4>
+          <p>A potential trigger is a food that was added within the 24-hour period preceding a reaction.  
+          These foods are sorted by frequency.  
+          Frequency is calculated based on the number of times the food preceded the reaction, divided by the number of times the food was eaten overall.</p>
         </div>
       </div>
     )
--- src/store.js
+++ src/store.js
@@ -6,12 +6,15 @@ const initialState = {};
 
 const middleware = [thunk];
 
+const devTools = process.env.NODE_ENV === 'development' ? 
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__() :
+  null
+
 const store = createStore(
   rootReducer,
   initialState,
   compose(
-    applyMiddleware(...middleware),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+    applyMiddleware(...middleware), devTools
   )
 );
 
