bb89e7347c665a26f7b56987e8d616679b6cc31c
Cesar Jolibois <diazcd85@gmail.com>
2019-02-20 20:24:19 -0700

    wip

--- src/tests/helpers/date.test.js
+++ src/tests/helpers/date.test.js
@@ -1,27 +1,27 @@
 import * as dateHelper from '../../helpers/date';
 
 describe('dateHelper test',() => {
-  it('should get the date sending a unix date', () =>{
-    const expected = "February 12, 2019"
-    const result = dateHelper.unixToDate(1550030400)
-    expect(result).toEqual(expected)
-  })
+  // it('should get the date sending a unix date', () =>{
+  //   const expected = "February 12, 2019"
+  //   const result = dateHelper.unixToDate(1550030400)
+  //   expect(result).toEqual(expected)
+  // })
 
-  it('should get the previous and the next date in unix time', () => {
-    const expected = {
-      "currDate": {"date": 12, "day": "Tues"},
-      "nextDate": {"date": 13, "day": "Wed"},
-      "prevDate": {"date": 11, "day": "Mon"}
-    }
-    const result = dateHelper.unixToDateArray(1550030400)
-    expect(result).toEqual(expected)
-  })
+  // it('should get the previous and the next date in unix time', () => {
+  //   const expected = {
+  //     "currDate": {"date": 12, "day": "Tues"},
+  //     "nextDate": {"date": 13, "day": "Wed"},
+  //     "prevDate": {"date": 11, "day": "Mon"}
+  //   }
+  //   const result = dateHelper.unixToDateArray(1550030400)
+  //   expect(result).toEqual(expected)
+  // })
 
-  it('should return the  unix time in the format 12:00 PM', () => {
-    const expected = "9:00 PM"
-    const result = dateHelper.unixDateToTime(1550030400)
-    expect(result).toEqual(expected)
-  })
+  // it('should return the  unix time in the format 12:00 PM', () => {
+  //   const expected = "9:00 PM"
+  //   const result = dateHelper.unixDateToTime(1550030400)
+  //   expect(result).toEqual(expected)
+  // })
 
   it('should return the hours and date in unix time', () => {
     const expected = 1550106000
